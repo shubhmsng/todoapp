b43f19600461f5b35edf3f42529d7db0b8d4a149
shubhmsng <shubhmsing@gmail.com>
2021-05-25 10:39:02 +0530

    added usememo and usecallback method

--- src/App.js
+++ src/App.js
@@ -1,36 +1,77 @@
-import { useState, useRef } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import TodoItem from './TodoItem';
 import './App.css';
 
+let count1 = 0;
+let count2 = 0;
+
 function App() {
   const [todo, setTodo] = useState([]);
-  const textbox = useRef(null);
+  const [todoName, setTodoName] = useState("");
 
-  const addNewTodo = () => {
-    if(textbox.current.value) {
+  const addNewTodo = useCallback(() => {
+    console.log("count1", ++count1);
+    if(todoName) {
       let new_todo = [...todo];
-      new_todo.push(textbox.current.value);
+      new_todo.push(todoName);
       setTodo(new_todo);
-      textbox.current.value = "";
+      setTodoName("");
     }
-  }
+  } , [todo, todoName]);
+
+  // const addNewTodo = () => {
+  //   console.log("count1", ++count1);
+  //   if(todoName) {
+  //     let new_todo = [...todo];
+  //     new_todo.push(todoName);
+  //     setTodo(new_todo);
+  //     setTodoName("");
+  //   }
+  // }
   
-  const removeTodo = name => {
+  const removeTodo = useMemo(() => name => {
+    console.log("count2", ++count2);
     if(name) {
       let new_todo = [...todo];
       new_todo = new_todo.filter(val => val !== name);
       setTodo(new_todo);
     }
-  }
+  }, [todo]);
+
+  // const removeTodo = (name) => {
+  //   console.log("count2", ++count2);
+  //   if(name) {
+  //     let new_todo = [...todo];
+  //     new_todo = new_todo.filter(val => val !== name);
+  //     setTodo(new_todo);
+  //   }
+  // }
+
+  const updateText = (e) => {
+    setTodoName(e.target.value);
+  };
  
   return (
     <div>
       <h1 className="title">To Do App</h1>
       <div className="App">
-        <input type="text" className="todo-textbox" placeholder="Add todo" ref={textbox}></input>
-        <button type="button" className="add-btn" onClick={addNewTodo}>Add</button>
+        <input 
+          type="text"
+          className="todo-textbox"
+          placeholder="Add todo"
+          onChange={updateText}
+          value={todoName} >
+        </input>
+        <button 
+          type="button"
+          className="add-btn"
+          onClick={addNewTodo}>
+          Add
+        </button>
       </div>
-      <TodoItem todo={todo} removeTodo={removeTodo}/>
+      <TodoItem 
+        todo={todo}
+        removeTodo={removeTodo}/>
     </div>
   );
 }
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-let count = 0;
+
 const TodoItem = ({ todo, removeTodo }) => {
-    console.log(++count);
     return(
         <ul>
             {todo?.map((val, index) => 
